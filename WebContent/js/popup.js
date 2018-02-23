5756830442a5aba998ec4e358d3799b4aceec2bc
H16304013 <fn16304013@ga.tera-house.ac.jp>
2018-02-23 14:20:58 +0900

    jsファイル修正

--- WebContent/js/popup.js
+++ WebContent/js/popup.js
@@ -23,36 +23,5 @@ $(function() {
 	$('#closeAddMember, #layer').click(function(e) {
 	    $('#addMemberPopup, #addMemberLayer').hide();
 	});
-
-// ポップアップscript
-//createGroupポップアップ
-$(function() {
-
-	$('#groupIcon').click(function(e) {// show popupボタンクリック時の処理
-	    $('#createGroupPopup, #createGroupLayer').show();
-	});
-
-	// レイヤー/ポップアップのcloseボタンクリック時の処理
-	$('#closeCreateGroup, #layer').click(function(e) {
-	    $('#createGroupPopup, #createGroupLayer').hide();
-	});
 });
-
-//addMemberポップアップ
-$(function() {
-
-	$('#addMember').click(function(e) {// show popupボタンクリック時の処理
-	    $('#addMemberPopup, #addMemberLayer').show();
-	});
-
-	// レイヤー/ポップアップのcloseボタンクリック時の処理
-	$('#closeAddMember, #layer').click(function(e) {
-	    $('#addMemberPopup, #addMemberLayer').hide();
-	});
-});
-
-
-
-
-
 
--- WebContent/js/search.js
+++ WebContent/js/search.js
@@ -43,49 +43,5 @@ $(function(){
 			}
 	     });
 	});
-
-$(document).ready(function() {
-	$('#follow').addClass("active");
-	$('#top').removeClass("active");
 });
 
-$(function(){
-	$('#grade, #departmentId, #test').change(function() {
-		var dept =  $('#departmentId').val();
-		var grade =  $('#grade').val();
-		var search = new RegExp($('#test').val().trim());
-
-		$('#table tbody tr').each(function(){
-		//td:eq(0) の０番目
-			var userDept = $(this).find("td:eq(3)").html();
-			var userGrade = $(this).find("td:eq(2)").html();
-			var username = $(this).find("td:eq(1)").html();
-
-			if(userDept.match(dept) && grade === "default" && search ===""){
-				$(this).show();
-			}
-			else if(userGrade.match(grade) && dept === "default" && search ===""){
-				$(this).show();
-			}
-			else if(username.match(search) && dept === "default" && grade === "default"){
-				$(this).show();
-			}
-			else if(userGrade.match(grade) && userDept.match(dept) && grade != "default" && dept != "default" && search ===""){
-				$(this).show();
-			}
-			else if(userGrade.match(grade) && username.match(search) && grade != "default" && search !="" && dept === "default"){
-				$(this).show();
-			}
-			else if(userDept.match(dept) && username.match(search) && dept != "default" && search !="" && grade === "default"){
-				$(this).show();
-			}
-			else if(userGrade.match(grade) && username.match(search) && userDept.match(dept) &&
-					grade != "default" && search !="" && dept != "default"){
-				$(this).show();
-			}
-			else{
-				$(this).hide();
-			}
-	     });
-	});
-})
